ec98798e041ad8b87a893f42630dd06dbf8398c3
Lauren Gifford <lauren.gifford.17@gmail.com>
2020-11-13 22:50:28 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,9 @@
 def find_element_index(array, value_to_find)
  counter = 0
  while counter < array.length do
-   array[counter] == value_to_find
+   if array[counter] == value_to_find
+     return counter
+   end
    counter += 1
  end
- array.index(value_to_find)
 end
